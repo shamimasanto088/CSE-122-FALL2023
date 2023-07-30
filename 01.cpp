813ba0dacfbbd7772627ff675c19f9234b9be1de
Shamima Akter Santo <140314083+shamimasanto088@users.noreply.github.com>
2023-07-30 14:59:54 +0600

    Add files via upload

---
+++ 01.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Welcome to the wonderful world of c++!!!\n";
+    cout<<"Lab1\t";
+    cout<<"\n END\t PROGRAM\t BYE";
+    cout<<"Welcome in yarmouk university\rHellooo";
+return 0;
+}
