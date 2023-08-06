9e55b73198e826cb17e71ea914297931c34a5395
Shamima Akter Santo <140314083+shamimasanto088@users.noreply.github.com>
2023-08-06 14:50:18 +0600

    Update Watermelon.cpp

--- Watermelon.cpp
+++ Watermelon.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int wm;
+    cin>>wm;
+    if(wm%2==1)
+    {
+        cout<<"NO"<<endl;
+
+    }
+    else if(wm==2)
+    {
+        cout<<"NO"<<endl;
+    }
+    else
+    {
+        cout<<"YES"<<endl;
+    }
+    return 0;
+}
 
