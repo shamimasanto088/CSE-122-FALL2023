a9b0f94387d551428b2b9d2475565caadb93979f
Shamima Akter Santo <140314083+shamimasanto088@users.noreply.github.com>
2023-08-13 15:03:20 +0600

    Update next round.c

--- next round.c
+++ next round.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+int main()
+{
+    int n,k;
+    scanf("%d %d",&n,&k);
+    int scores[n];
+    for (int i=0; i<n; i++)
+    {
+        scanf("%d",&scores[i]);
+    }
+    int count =0;
+    int kth_score=scores[k-1];
+    for (int i=0; i<n; i++)
+    {
+        if (scores[i]>=kth_score && scores[i]>0)
+        {
+            count++;
+        }
+    }
+    printf("%d\n",count);
+    return 0;
+}
